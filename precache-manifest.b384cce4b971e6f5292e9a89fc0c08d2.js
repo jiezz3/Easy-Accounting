764fcbb1d107b0fa85b58026d7bbdf9c05eaac37
jiezz3 <yuanweijie0769@gmail.com>
2021-08-11 15:25:15 +0800

    deploy

---
+++ precache-manifest.b384cce4b971e6f5292e9a89fc0c08d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d51d2272f7f7fd3a43f1",
+    "url": "/Easy-Accounting/css/app.31e0c21c.css"
+  },
+  {
+    "revision": "2ecb996cd093f085f2a3f06cd7f36a75",
+    "url": "/Easy-Accounting/index.html"
+  },
+  {
+    "revision": "d51d2272f7f7fd3a43f1",
+    "url": "/Easy-Accounting/js/app.ac3543ca.js"
+  },
+  {
+    "revision": "467129fd2dd3599570fe",
+    "url": "/Easy-Accounting/js/chunk-vendors.87625c90.js"
+  },
+  {
+    "revision": "1e0e1af34bbc4494c22f6dad8591c23b",
+    "url": "/Easy-Accounting/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/Easy-Accounting/robots.txt"
+  }
+]);
